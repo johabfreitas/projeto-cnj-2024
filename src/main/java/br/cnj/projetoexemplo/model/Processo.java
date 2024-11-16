fef569ed7a7e0bf49b80021ea5581c7f7a2427ca
johabfreitas <freitasjohab@gmail.com>
2024-11-15 23:37:26 -0300

    Criada a classe Processo

--- src/main/java/br/cnj/projetoexemplo/model/Processo.java
+++ src/main/java/br/cnj/projetoexemplo/model/Processo.java
@@ -1,5 +1,20 @@
 package br.cnj.projetoexemplo.model;
 
+import java.time.LocalDateTime;
+
 public class Processo {
+	
+	private int processo;
+	private int processoOrigem;
+	private String complemento;
+	private LocalDateTime inicio;
+	private int jbpm;
+	private LocalDateTime fim;
+	private LocalDateTime duracao;
+	private int actor;
+	private String cadastroProcesso;
+	private int processoPG;
+	private int sessaoPG;
+	private int processoTemp;
 
 }
